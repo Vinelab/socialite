b93beaab69b46601b6adf24d97f46f26a18886cf
Abed Halawi <halawi.abed@gmail.com>
2015-08-11 18:57:44 +0300

    implement AppAuth trait and interface

--- src/OAuth/AppAuthProviderInterface.php
+++ src/OAuth/AppAuthProviderInterface.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace Vinelab\Socialite\OAuth;
+
+interface AppAuthProviderInterface
+{
+    /**
+     * Fetch a post from the specified social network.
+     *
+     * @param string $id
+     *
+     * @return array
+     */
+    public function post($id);
+}
